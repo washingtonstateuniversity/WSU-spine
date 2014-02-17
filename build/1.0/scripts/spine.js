ae97bac36aec4f70fb6b6ae1c58a728ee80b9016
jeremyBass <jeremybass@cableone.net>
2014-02-17 15:08:17 -0800

    moves the auto complete to it's own file
    This apart of moving all plugin parts out of the controller file

--- build/1.0/scripts/spine.js
+++ build/1.0/scripts/spine.js
@@ -4,37 +4,6 @@ function dump(n,t){var i="",f,e,r,u;for(t||(t=0),f="",e=0;e<t+1;e++)f+=" ";if(ty
 (function($){
 	"use strict";
 	function setup_search(){
-		$.widget( "ui.autosearch", $.ui.autocomplete, {
-			_renderMenu: function( ul, items ) {
-				var that	= this;
-				var matched	= items.filter(function(obj, item) { if(obj.related=="false"){return item;} });//note this should be a single loop?
-				var related	= items.filter(function(obj, item) { if(obj.related!="false"){return item;} });
-
-				$.each(matched, function(i, item) { that._renderItemData( ul, item ); });
-				
-				if(this.options.showRelated && related.length){
-					if(this.options.relatedHeader){
-						that._renderHeader( ul, this.options.relatedHeader );
-					}
-					$.each( related, function(i, item) { that._renderItemData( ul, item ); });
-				}
-			},
-			_renderItemData: function( ul, item ) {
-				return this._renderItem( ul, item ).data( "ui-autocomplete-item", item );
-			},
-			_renderItem: function( ul, item ) {
-				var text	= item.label;
-				var value	= item.value;
-				var regex	= "(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)";
-					text	= "<a href='"+value+"'>" + text.replace( new RegExp( regex , "gi" ), "<strong>$1</strong>" )+"</a>";
-	
-				return $( "<li></li>" ).data( "item.autocomplete", item ).append( text ).appendTo( ul );
-			},
-			_renderHeader: function( ul, text ) {
-				return $( "<li></li>" ).append( "<a href=''>"+text+"</a>" ).appendTo( ul );
-			}
-		});
-	
 		/* Search autocomplete */
 		var cur_search = "";
 		var termTemplate = "<strong>%s</strong>";
