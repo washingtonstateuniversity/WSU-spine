4d83d3a565ed29fb33d93aca06a8e8d5d56800d1
jeremyBass <jeremybass@cableone.net>
2014-02-18 07:56:36 -0800

    example of the extension

---
+++ scripts/ui.spine.search.js
@@ -0,0 +1,13 @@
+ /*!
+ *
+ * Depends:
+ *		jquery.ui.v.js
+ */
+( function($) {
+	$.extend($.ui.spine.prototype, {
+        foo: function(jObj, options) {
+                //do stuff
+                return jObj;
+            },
+	});
+} (jQuery) );
