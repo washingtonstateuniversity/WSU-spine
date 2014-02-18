318cf34dceb9ab2ab43979da549ad9fe6ef4b3c6
jeremyBass <jeremybass@cableone.net>
2014-02-18 09:07:08 -0800

    adds the used files for the spine to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 		      dest: 'build/spine.css',
 		    },
 		    scripts: {
-		      src: [ 'scripts/debug.js','scripts/wsu_autocomplete.js','scripts/jQuery.spine.js','scripts/spine.js'],
+		      src: [ 'scripts/debug.js','scripts/wsu_autocomplete.js','scripts/ui.spine.js','scripts/ui.spine.framework.js','scripts/ui.spine.search.js','scripts/spine.js'],
 			  dest: 'build/spine.js',
 			  },
 		},
