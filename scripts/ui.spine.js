ffe87817ba8dac69802f4011d952f266772df92a
jeremyBass <jeremybass@cableone.net>
2014-02-18 08:09:52 -0800

    moves the search to the plugin extension

--- scripts/ui.spine.js
+++ scripts/ui.spine.js
@@ -79,72 +79,7 @@
 			self._call(self.options.callback, self.instance.spine);
 		},
         
-        setup_search: function (){
-            /* Search autocomplete */
-            var cur_search = "";
-            var termTemplate = "<strong>%s</strong>";
-            var term = "";
-            $( "#wsu-search input[type=text]" ).autosearch({
-                source: function( request, response ) {
-                    term = request.term;
-                    $.ajax({
-                        url: "http://search.wsu.edu/2013service/searchservice/search.asmx/AZSearch",
-                        dataType: "jsonp",
-                        data: {
-                            featureClass: "P",
-                            style: "full",
-                            maxRows: 12,
-                            name_startsWith: request.term
-                        },
-                        success: function( data, status, xhr  ) {
-                            var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-                            response( $.map( data, function( item ) {
-                                var text = item.label;
-                                if ( (item.value && ( !request.term || matcher.test(text)) || item.related == "true" ) ){
-                                    return {
-                                        label: item.label,
-                                        value: item.value,
-                                        searchKeywords: item.searchKeywords,
-                                        related: item.related
-                                    };
-                                }
-                            }));
-                        }
-                    });
-                },
-                search: function(event, ui) {
-                    /**/
-                },
-                appendTo: "#spine-shortcuts",
-                showRelated:true,
-                relatedHeader:"<hr/>",
-                minLength: 2,
-                select: function( e, ui ) {
-                    var id = ui.item.searchKeywords;
-                    var term = ui.item.label;
-                    $("#indices").empty();
-                    // to come back later to
-                    /*
-                    var url=siteroot+"public/get_place.castle";
-                    if ( e.which != 13 ){
-                        if(typeof($.jtrack)!=="undefined")$.jtrack.trackPageview(pageTracker,url+(id!=""?'?id='+id:'')+(term!=""?'&term='+term:''));
-                        getSignlePlace(jObj,id);
-                    }
-                    */
-                    $( "#wsu-search input#searchterm[type=text]" ).autocomplete("close");
-                },
-                focus: function( event, ui ) {
-                    // to come back later to
-                    //$( "#wsu-search input#searchterm[type=text]" ).val( ui.item.label );
-                    //return false;
-                },
-                open: function(e,ui) {
-                    // to come back later to
-                    //$('.ui-autocomplete.ui-menu').removeClass( "ui-corner-all" );
-                }
-            }).data( "autocomplete" );
-        },
-        
+
         
         
 		/**
@@ -167,9 +102,7 @@
 				}
 			}
 		},
-        
-        
-        
+
 		/**
 		 * Helper method for unwrapping jQuery/DOM/string elements
 		 * @param obj:string/node/jQuery
