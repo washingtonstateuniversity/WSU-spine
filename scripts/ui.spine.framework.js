ee58d3c552003d63967f40adbf89a513adaf6454
jeremyBass <jeremybass@cableone.net>
2014-02-18 08:35:10 -0800

    sets up the framework context file

---
+++ scripts/ui.spine.framework.js
@@ -0,0 +1,17 @@
+ /*!
+ *
+ * Depends:
+ *		jquery.ui.v.js
+ */
+( function($) {
+	$.extend($.ui.spine.prototype, {
+        foo: function(jObj, options) {
+                //do stuff
+                return jObj;
+            },
+        _init: function(jObj, options) {
+            
+        }
+        
+	});
+} (jQuery) );
