69442329425efa1a4703c05a61ce1f8b05000fc6
jeremyBass <jeremybass@cableone.net>
2014-02-18 07:24:48 -0800

    set up a basic outline of the options needed

--- scripts/spine.js
+++ scripts/spine.js
@@ -369,7 +369,32 @@
 
         $.spine("search",{});
         $.spine({
+            "search":{
+                "providers":{
+                    "local":{
+                        url: "http://search.wsu.edu/2013service/searchservice/search.asmx/AZSearch",
+                        dataType: "jsonp",
+                        featureClass: "P",
+                        style: "full",
+                        maxRows: 12
+                    } 
+                },
+                "result":{
+                    appendTo: "#spine-shortcuts",
+                    showRelated:true,
+                    relatedHeader:"<hr/>",
+                    minLength: 2,   
+                }
+            },
+            "framework":{
             
+            },
+            "social":{
+            
+            },
+            "analytics":{
+            
+            }
         });
         
         
