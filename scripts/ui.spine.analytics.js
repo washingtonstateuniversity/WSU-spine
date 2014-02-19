2902cac287e890972e0620ef83d52216bb5bbc13
jeremyBass <jeremybass@cableone.net>
2014-02-19 10:43:32 -0800

    add the spine analytics extension

---
+++ scripts/ui.spine.analytics.js
@@ -0,0 +1,13 @@
+ /*!
+ *
+ * Depends:
+ *		jquery.ui.spine.js
+ */
+( function($) {
+	$.extend($.ui.spine.prototype, {
+        analytics_init: function(options) {
+            alert('init analytics');
+            alert("options==>"+dump(options));
+        },
+	});
+} (jQuery) );
