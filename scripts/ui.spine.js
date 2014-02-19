16562d53c53cd26c2a7ca0de4246d6cba827258e
jeremyBass <jeremybass@cableone.net>
2014-02-19 12:07:51 -0800

    improves sizing func by adding $.fn.stripClass
    This shortened the code a lot

--- scripts/ui.spine.js
+++ scripts/ui.spine.js
@@ -4,6 +4,27 @@
  *	});
 **/
 ;(function ( $, window, document, undefined ) {
+    
+    
+    $.fn.stripClass = function (partialMatch, endOrBegin) {
+        /// <summary>
+        /// The way removeClass should have been implemented -- accepts a partialMatch (like "btn-") to search on and remove
+        /// </summary>
+        /// <param name="partialMatch">the class partial to match against, like "btn-" to match "btn-danger btn-active" but not "btn"</param>
+        /// <param name="endOrBegin">omit for beginning match; provide a 'truthy' value to only find classes ending with match</param>
+        /// <returns type=""></returns>
+        var x = new RegExp((!endOrBegin ? "\\b" : "\\S+") + partialMatch + "\\S*", 'g');
+    
+        // http://stackoverflow.com/a/2644364/1037948
+        this.attr('class', function (i, c) {
+            if (!c) return; // protect against no class
+            return c.replace(x, '');
+        });
+        return this;
+    };
+    
+    
+    
 	/**
      * Sets up the plugins prototype
 	 * @param name:string
