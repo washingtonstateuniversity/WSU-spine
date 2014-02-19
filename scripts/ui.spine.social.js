95ebbad3a50993065bbcf170a56cdb789fd25ce0
jeremyBass <jeremybass@cableone.net>
2014-02-19 12:33:30 -0800

    adds social_options as a default

--- scripts/ui.spine.social.js
+++ scripts/ui.spine.social.js
@@ -8,6 +8,19 @@
         social_init: function(options) {
             //alert('init social');
             //alert("options==>"+dump(options));
+            $.extend(this.social_options,options);
+            //alert("options==>"+dump(this.social_options));
+            this.social_create();
         },
+        social_options:{
+            provider:"WSU_share",
+            script_url:"//repo.wsu.edu/wsu_share/1/",
+            channels:[
+                "facebook",
+                "twitter",
+                "Pintrest"
+            ]
+        },
+        social_create: function(){}
 	});
 } (jQuery) );
