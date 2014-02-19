1f612afb96bf511c91af430f44cfb904c035b019
jeremyBass <jeremybass@cableone.net>
2014-02-18 16:38:15 -0800

    work on bettering the debug

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,8 +1,9 @@
 // @if DEBUG
 /* for debug only. remove when done */
 
-function dump(arr,level) {
+function dump(arr,limit,level) {
 	var dumped_text = "";
+    if(!limit) limit=10;
 	if(!level) level = 0;
 	
 	//The padding given at the beginning of the line.
@@ -10,16 +11,18 @@ function dump(arr,level) {
 	for(var j=0;j<level+1;j++) level_padding += "    ";
 	
 	if(typeof(arr) == 'object') { //Array/Hashes/Objects 
-		for(var item in arr) {
-			var value = arr[item];
-			
-			if(typeof(value) == 'object') { //If it is an array,
-				dumped_text += level_padding + "'" + item + "' ...\n";
-				dumped_text += dump(value,level+1);
-			} else {
-				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-			}
-		}
+        if(level<=limit){
+            for(var item in arr) {
+                var value = arr[item];
+                
+                if(typeof(value) == 'object') { //If it is an array,
+                    dumped_text += level_padding + "'" + item + "' ...\n";
+                    dumped_text += dump(value,limit,level+1);
+                } else {
+                    dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+                }
+            }
+        }
 	} else { //Stings/Chars/Numbers etc.
 		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
 	}
