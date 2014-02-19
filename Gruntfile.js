8751b02776074b04c6b5b2dcffb0537a37b222b7
jeremyfelt <jeremy.felt@gmail.com>
2014-02-19 15:37:52 -0800

    Cleanup mixed tabs/spaces
    Tidy. :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,25 @@
 module.exports = function(grunt) {
-
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		concat: {
 			styles: {
-		      src: ['styles/skeleton.css','styles/colors.css','styles/spine.css','styles/respond.css'],
-		      dest: 'build/<%= pkg.build_version %>/spine.css',
-		    },
-		    scripts: {
-		      src: [ 
-                    'scripts/debug.js',
-                    'scripts/wsu_autocomplete.js',
-                    'scripts/ui.spine.js',
-                    'scripts/ui.spine.framework.js',
-                    'scripts/ui.spine.search.js',
-                    'scripts/ui.spine.social.js',
-                    'scripts/ui.spine.analytics.js',
-                    'scripts/spine.js'
-                   ],
-			  dest: 'build/<%= pkg.build_version %>/spine.js',
-			  },
+				src: ['styles/skeleton.css','styles/colors.css','styles/spine.css','styles/respond.css'],
+				dest: 'build/<%= pkg.build_version %>/spine.css',
+			},
+			scripts: {
+				src: [
+					'scripts/debug.js',
+					'scripts/wsu_autocomplete.js',
+					'scripts/ui.spine.js',
+					'scripts/ui.spine.framework.js',
+					'scripts/ui.spine.search.js',
+					'scripts/ui.spine.social.js',
+					'scripts/ui.spine.analytics.js',
+					'scripts/spine.js'
+				],
+				dest: 'build/<%= pkg.build_version %>/spine.js',
+			},
 		},
 		uglify: {
 			options: {
@@ -33,26 +32,26 @@ module.exports = function(grunt) {
 			}
 		},
 		cssmin: {
-		  combine: {
-		    files: {
-		    // Hmmm, in reverse order
-		      'build/<%= pkg.build_version %>/spine.min.css': ['build/<%= pkg.build_version %>/spine.css']
-		    }
-		  }
+			combine: {
+				files: {
+					// Hmmm, in reverse order
+					'build/<%= pkg.build_version %>/spine.min.css': ['build/<%= pkg.build_version %>/spine.css']
+				}
+			}
 		},
 		copy: {
-		  main: {
-		    files: [
-		      {expand: true, dot:true, src: ['fonts/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['html/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, dot:true, src: ['icons/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['images/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['marks/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['scripts/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['styles/*'], dest: 'build/<%= pkg.build_version %>/'},
-		      {expand: true, src: ['spine.html','spine.min.html','authors.txt','favicon.ico'], dest: 'build/<%= pkg.build_version %>/'},
-		    ]
-		  }
+			main: {
+				files: [
+					{expand: true, dot:true, src: ['fonts/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['html/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, dot:true, src: ['icons/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['images/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['marks/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['scripts/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['styles/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['spine.html','spine.min.html','authors.txt','favicon.ico'], dest: 'build/<%= pkg.build_version %>/'},
+				]
+			}
 		},
 		jshint: {
 			files: ['Gruntfile.js', 'scripts/*.js'],
@@ -66,19 +65,18 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-        preprocess : {
-            options: {
-                inline: true,
-                context : {
-                    DEBUG: true
-                }
-            },
-            js : {
-                src: 'build/<%= pkg.build_version %>/spine.js'
-            }
-        }
+		preprocess : {
+			options: {
+				inline: true,
+				context : {
+					DEBUG: true
+				}
+			},
+			js : {
+				src: 'build/<%= pkg.build_version %>/spine.js'
+			}
+		}
 	});
-	
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -86,9 +84,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-preprocess');
-        
+	grunt.loadNpmTasks('grunt-preprocess');
+
 	// Default task(s).
 	grunt.registerTask('default', ['concat','preprocess:js','cssmin','uglify','copy']);
-
 };
