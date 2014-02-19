043f4775db181d7f8040f7f1a47abe3dc1edbd9e
jeremyBass <jeremybass@cableone.net>
2014-02-19 12:34:41 -0800

    adds analytics_options defaults

--- scripts/ui.spine.analytics.js
+++ scripts/ui.spine.analytics.js
@@ -8,6 +8,21 @@
         analytics_init: function(options) {
             //alert('init analytics');
             //alert("options==>"+dump(options));
+            $.extend(this.analytics_options,options);
+            //alert("options==>"+dump(this.analytics_options));
+            this.analytics_create();
         },
+        analytics_options:{ 
+            "google":{
+                analytics:{
+                    "account_id":"UA-XXXXX-XX"
+                },
+                verification:"XXXXXXXXXXXXXXXXXXXXX"
+            },
+            "bing":{
+                verification:"XXXXXXXXXXXXXXXXXXXXX"
+            }
+        },
+        analytics_create: function(){}
 	});
 } (jQuery) );
