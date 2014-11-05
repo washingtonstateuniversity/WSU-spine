f66d87d7d046bc419f91cc774881a5450cd54482
jeremyfelt <jeremy.felt@gmail.com>
2014-11-05 09:01:06 -0800

    Apply active class only to parents of #spine child navigation
    Our wildcard search for `*=active` could target other navigation
    list items outside of `#spine`. We should avoid stomping on things
    like that.

--- scripts/ui.spine.framework.js
+++ scripts/ui.spine.framework.js
@@ -384,8 +384,8 @@
 			// NAVIGATION
 			// Tag location and hierarchy
 			$("#spine nav ul,#spine ul").parents("li").addClass("parent");
-			$("#spine nav li[class*=current], nav li[class*=active]").addClass("active").parents("li").addClass("active");
-			$("#spine nav li a[class*=current], nav li a[class*=active]").parents("li").addClass("active");
+			$("#spine nav li[class*=current], #spine nav li[class*=active]").addClass("active").parents("li").addClass("active");
+			$("#spine nav li a[class*=current], #spine nav li a[class*=active]").parents("li").addClass("active");
 			$("#spine .active:not(:has(.active))").addClass("dogeared");
 
 			// Couplets
