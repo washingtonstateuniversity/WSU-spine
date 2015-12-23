eea6b74b7daf845f50c816d11aa622d40cd8a733
jeremyfelt <jeremy.felt@gmail.com>
2015-12-22 23:09:43 -0800

    Update Gruntfile.js and csslint.js to meet style guide, jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,66 @@
-
-module.exports = function(grunt) {
+/* globals require */
+module.exports = function( grunt ) {
 	// Look at the passed path for .js files used as extended Grunt config.
-	function loadConfig(path) {
-		var glob = require('glob');
-
+	function loadConfig( path ) {
+		var glob = require( "glob" );
 		var object = {};
 		var key;
 
-		glob.sync('*', {cwd: path}).forEach(function(option) {
-			key = option.replace(/\.js$/,'');
-			object[key] = require(path + option);
-		});
+		glob.sync( "*", { cwd: path } ).forEach( function( option ) {
+			key = option.replace( /\.js$/, "" );
+			object[ key ] = require( path + option );
+		} );
 
 		return object;
 	}
 
 	var pkg,setbase,config;
 
-	pkg = grunt.file.readJSON('package.json');
-	setbase = grunt.option('setbase') || pkg.build_location+'/'+pkg.build_version+'/';
+	pkg = grunt.file.readJSON( "package.json" );
+	setbase = grunt.option( "setbase" ) || pkg.build_location + "/" + pkg.build_version + "/";
 
 	config = {
 		pkg: pkg,
 		setbase:setbase,
 		config: {
-			build: 'build'
+			build: "build"
 		}
 	};
 
-	grunt.util._.extend(config, loadConfig('./tasks/options/'));
-	grunt.initConfig(config);
+	grunt.util._.extend( config, loadConfig( "./tasks/options/" ) );
+	grunt.initConfig( config );
 
-	require('load-grunt-tasks')(grunt);
-	grunt.loadTasks('tasks');
+	require( "load-grunt-tasks" )( grunt );
+	grunt.loadTasks( "tasks" );
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask( "default", [ "jshint" ] );
 
-	grunt.registerTask('prod', [
-		'env:prod',
-		'build',
-		'build_tests',
-		'copy:main',
-		'copy:dev'
-	]);
+	grunt.registerTask( "prod", [
+		"env:prod",
+		"build",
+		"build_tests",
+		"copy:main",
+		"copy:dev"
+	] );
 
-	grunt.registerTask('dev', [
-		'jshint',
-		'env:dev',
-		'build',
-		'build_tests',
-		'copy:main',
-		'copy:dev'
-	]);
+	grunt.registerTask( "dev", [
+		"jshint",
+		"env:dev",
+		"build",
+		"build_tests",
+		"copy:main",
+		"copy:dev"
+	] );
 
-	grunt.registerTask('build', [
-		'clean',
-		'sass:dev',
-		'concat',
-		'preprocess:js',
-		'autoprefixer',
-		'csslint',
-		'cssmin',
-		'uglify'
-	]);
+	grunt.registerTask( "build", [
+		"clean",
+		"sass:dev",
+		"concat",
+		"preprocess:js",
+		"autoprefixer",
+		"csslint",
+		"cssmin",
+		"uglify"
+	] );
 };
--- tasks/options/csslint.js
+++ tasks/options/csslint.js
@@ -1,32 +1,32 @@
 module.exports = {
 	spine: {
-		src: ["<%= config.build %>/spine.css"],
+		src: [ "<%= config.build %>/spine.css" ],
 		options: {
-			'fallback-colors': false, // unless we want to support IE8
-			'box-sizing': false,      // unless we want to support IE7
-			'ids': false,
-			'regex-selectors': false,
-			'adjoining-classes': false,
-			'box-model': false,          // audit
-			'universal-selector': false, // audit
-			'unique-headings': false,    // audit
-			'qualified-headings': false, // audit
-			'outline-none': false,       // audit
-			'known-properties': false,
-			'floats': false,
-			'star-property-hack': 2,
-			'text-indent': true,
-			'font-sizes': true,
-			'display-property-grouping': true,
-			'shorthand': false,                    // true
-			'duplicate-properties': false,         // true
-			'compatible-vendor-prefixes': false,   // 2
-			'overqualified-elements': false,       // 2
-			'duplicate-background-images': false,  // 2
-			'important': false,                    // 2
-			'empty-rules': false,                  // 2
-			'vendor-prefix': false,                // 2
-			'zero-units': false                    // 2
+			"fallback-colors": false, // unless we want to support IE8
+			"box-sizing": false,      // unless we want to support IE7
+			"ids": false,
+			"regex-selectors": false,
+			"adjoining-classes": false,
+			"box-model": false,          // audit
+			"universal-selector": false, // audit
+			"unique-headings": false,    // audit
+			"qualified-headings": false, // audit
+			"outline-none": false,       // audit
+			"known-properties": false,
+			"floats": false,
+			"star-property-hack": 2,
+			"text-indent": true,
+			"font-sizes": true,
+			"display-property-grouping": true,
+			"shorthand": false,                    // true
+			"duplicate-properties": false,         // true
+			"compatible-vendor-prefixes": false,   // 2
+			"overqualified-elements": false,       // 2
+			"duplicate-background-images": false,  // 2
+			"important": false,                    // 2
+			"empty-rules": false,                  // 2
+			"vendor-prefix": false,                // 2
+			"zero-units": false                    // 2
 		}
 	}
 };
