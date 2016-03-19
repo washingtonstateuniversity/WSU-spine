a281f7a7d49c0def5ed80692a67bcd3d6f97828e
jeremyfelt <jeremy.felt@gmail.com>
2016-03-18 17:20:40 -0700

    Update initial ruleset for JSCS

--- tasks/options/jscs.js
+++ tasks/options/jscs.js
@@ -3,7 +3,10 @@ module.exports = {
         src: "scripts/*.js",
         options: {
             preset: "jquery",
-            verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+            verbose: true,                                 // Display the rule name with the warning.
+            requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+            maximumLineLength: 250,                        // temporary
+            disallowMultipleLineStrings: false             // temporary
         }
     }
 };
