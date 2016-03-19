f600a2e622b2d88d9c975f615a566dd51394e0ff
jeremyfelt <jeremy.felt@gmail.com>
2016-03-18 17:01:11 -0700

    Add an initial JSCS configuration
    This will help enforce a clean JavaScript code base using the
    standards defined by the jQuery project.

---
+++ tasks/options/jscs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    spine_files : {
+        src: "scripts/*.js",
+        options: {
+            preset: "jquery",
+            verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+        }
+    }
+};
