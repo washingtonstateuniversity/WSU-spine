008df2f381d96a240f08cebce9e8bf47daa7f024
Jeremy Felt <jeremy.felt@gmail.com>
2016-04-15 17:00:27 -0700

    Fix code style after fs-extra add

--- tasks/build_tests.js
+++ tasks/build_tests.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.registerTask("build_tests", "Set up all test pages", function() {
 		var fs = require("fs");
 		var extend = require("extend");
-		var fs = require('fs-extra')
+		var fs = require("fs-extra");
 		var nunjucks = require("nunjucks");
 		var env = nunjucks.configure("test");
 		env.addFilter("indexof", function(str, cmpstr) {
