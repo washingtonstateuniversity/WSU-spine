0a1195b8b8352d2c104d177ab14d3e1b0b5bfb8f
Jeremy Felt <jeremy.felt@gmail.com>
2017-04-05 17:14:35 -0700

    Remove `verbose` rule, now enabled by default in JSCS

--- tasks/options/jscs.js
+++ tasks/options/jscs.js
@@ -3,7 +3,6 @@ module.exports = {
         src: "scripts/*.js",
         options: {
             preset: "jquery",
-            verbose: true,                                 // Display the rule name with the warning.
             requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
             maximumLineLength: 250,                        // temporary
         }
